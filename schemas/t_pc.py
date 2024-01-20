cb64b58550c81e3b9ad9d46fb956c81d70568675
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-20 22:57:15 +0900

    feat: pc詳細取得処理のレスポンススキーマの実装

--- schemas/t_pc.py
+++ schemas/t_pc.py
@@ -29,6 +29,28 @@ class createPc(BaseModel):
     class Config:
         orm_mode = True
 
+class detailPc(BaseModel):
+    id: int
+    label_name: Optional[str] = None
+    pc_name: Optional[str] = None
+    user_name: Optional[str] = None
+    pc_user: Optional[str] = None
+    condition: Optional[str] = None
+    manufacturer: Optional[str] = None
+    type: Optional[str] = None
+    service_tag: Optional[str] = None
+    os: Optional[str] = None
+    bit: int = None
+    ie_version: Optional[str] = None
+    ip_address: Optional[str] = None
+    gx_wwp_license: Optional[str] = None
+    delivery_date: Optional[date] = None
+    disposal_date: Optional[date] = None
+    remarks: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
 class pc(BaseModel):
     id: int
     label_name: Optional[str] = None
