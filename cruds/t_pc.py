ecc492a7e79771ff5070fe63807d5c8884555b0d
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-20 22:58:10 +0900

    feat: pc_id に紐づくデータを取得する処理の実装

--- cruds/t_pc.py
+++ cruds/t_pc.py
@@ -6,3 +6,7 @@ import models.t_pc as pc_model
 # PC一覧取得
 def get_pc(db: Session):
     return db.query(pc_model.T_pc).all()
+
+# PC詳細取得
+def get_detail_pc(db: Session, pc_id: int):
+    return db.query(pc_model.T_pc).filter(pc_model.T_pc.id == pc_id).first()
