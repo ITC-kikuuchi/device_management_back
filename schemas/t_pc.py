ee87793ad6566f297698b8f94dd1597bece7b44a
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-20 17:53:26 +0900

    feat: pc情報を追加する際に参照するcreate スキーマの実装

--- schemas/t_pc.py
+++ schemas/t_pc.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Optional
 
 from pydantic import BaseModel
@@ -6,6 +7,29 @@ from pydantic import BaseModel
 BaseModel は FastApi のスキーマモデルであることを示す。
 Pc クラスは BaseModel を継承しているクラス
 """
+
+class createPc(BaseModel):
+    label_name: Optional[str] = None
+    pc_name: Optional[str] = None
+    user_name: Optional[str] = None
+    pc_user: Optional[str] = None
+    condition: Optional[str] = None
+    manufacturer: Optional[str] = None
+    type: Optional[str] = None
+    service_tag: Optional[str] = None
+    os: Optional[str] = None
+    bit: int = None
+    ie_version: Optional[str] = None
+    ip_address: Optional[str] = None
+    gx_wwp_license: Optional[str] = None
+    delivery_date: Optional[date] = None
+    disposal_date: Optional[date] = None
+    remarks: Optional[str] = None
+    delete_flag: bool
+
+    class Config:
+        orm_mode = True
+
 class pc(BaseModel):
     id: int
     label_name: Optional[str] = None
