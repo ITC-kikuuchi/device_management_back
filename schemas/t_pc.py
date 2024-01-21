a4408885e8e66de208313716f60af0e264691758
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-21 20:05:21 +0900

    feat: pc 情報を更新する際に参照するスキーマの実装

--- schemas/t_pc.py
+++ schemas/t_pc.py
@@ -62,3 +62,24 @@ class detailPc(BaseModel):
 
     class Config:
         orm_mode = True
+
+class updatePc(BaseModel):
+    label_name: Optional[str] = None
+    pc_name: Optional[str] = None
+    user_name: Optional[str] = None
+    pc_user: Optional[str] = None
+    condition: Optional[str] = None
+    manufacturer: Optional[str] = None
+    type: Optional[str] = None
+    service_tag: Optional[str] = None
+    os: Optional[str] = None
+    bit: int = None
+    ie_version: Optional[str] = None
+    ip_address: Optional[str] = None
+    gx_wwp_license: Optional[str] = None
+    delivery_date: Optional[date] = None
+    disposal_date: Optional[date] = None
+    remarks: Optional[str] = None
+
+    class Config:
+        orm_mode = True
