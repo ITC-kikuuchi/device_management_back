01b15e2caf797d669002593bdb443baba19461c2
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-23 21:37:54 +0900

    feat: ログイン情報取得処理の実装

--- routers/auth.py
+++ routers/auth.py
@@ -9,6 +9,17 @@ import models.m_user as M_user
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
+    user = auth_crud.getUserByName(db, user_name=token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
 
 # ログインAPI
 @router.post("/login")
@@ -18,6 +29,10 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
         raise HTTPException(status_code=401, detail=f'メールアドレスまたはパスワードが違います。')
     return {"access_token": user.user_name, "token_type": "bearer"}
 
+# 　ログイン情報取得API
+@router.get("/me")
+async def me(current_user: M_user = Depends(get_current_user)):
+    return current_user
 
 @router.post("/logout")
 async def logout():
