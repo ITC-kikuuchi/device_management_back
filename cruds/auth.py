a5d97ee78daa1882600af87d642385bfc45db394
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-23 21:37:37 +0900

    fix: コメントの修正

--- cruds/auth.py
+++ cruds/auth.py
@@ -9,7 +9,7 @@ def getUser(db: Session, user: str, password: str):
         user_model.M_user.password == password
         ).first()
 
-# id に紐づくユーザ取得
+# トークンに紐づくユーザ取得
 def getUserByName(db: Session, user_name: str):
     return db.query(user_model.M_user).filter(
         user_model.M_user.user_name == user_name
