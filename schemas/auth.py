ca8c045acf2c65386cefc0dce88ebe69e28cd926
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-26 09:31:41 +0900

    feat: ログイン情報取得API のレスポンススキーマの実装

--- schemas/auth.py
+++ schemas/auth.py
@@ -5,3 +5,10 @@ from pydantic import BaseModel
 class user(BaseModel):
     mail_address: Optional[str] = None
     password: Optional[str] = None
+
+class loginUser(BaseModel):
+    id: int
+    user_name: Optional[str]
+
+    class Config:
+        orm_mode = True
