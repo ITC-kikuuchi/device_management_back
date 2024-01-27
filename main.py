ea6fef8c0e19952ad51ec101256bf61131d8cdfb
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-27 18:55:14 +0900

    feat: appに iosapi 用のサブルータの定義

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from routers import auth, pc
+from routers import auth, pc, ios
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(pc.router)
+app.include_router(ios.router)
