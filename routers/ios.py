8eacb67973c10645ffd84aad478dec563caf8f23
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-27 19:50:38 +0900

    feat: ios一覧取得API の実装

--- routers/ios.py
+++ routers/ios.py
@@ -7,3 +7,11 @@ import schemas.t_ios as ios_schema
 import cruds.t_ios as ios_crud
 
 router=APIRouter()
+
+# iOS一覧取得API
+@router.get("/ios", response_model=list[ios_schema.ios])
+def getIos(login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    try:
+        return ios_crud.getIos(db)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
