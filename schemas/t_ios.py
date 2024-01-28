13c571b10097bafdd3050abbce21ff11b322ccde
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-28 19:29:57 +0900

    feat: iOS登録処理のリクエストスキーマの実装

--- schemas/t_ios.py
+++ schemas/t_ios.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import date
 
 from pydantic import BaseModel
 
@@ -15,6 +16,21 @@ class ios(BaseModel):
     type: Optional[str] = None
     os: Optional[str] = None
     delete_flag: bool
+
+    class Config:
+        orm_mode = True
+
+class createIos(BaseModel):
+    label_name: Optional[str] = None
+    ios_name: Optional[str] = None
+    manufacturer: Optional[str] = None
+    type: Optional[str] = None
+    os: Optional[str] = None
+    carrier: Optional[str] = None
+    condition: Optional[str] = None
+    delivery_date: Optional[date] = None
+    disposal_date: Optional[date] = None
+    remarks: Optional[str] = None
 
     class Config:
         orm_mode = True
