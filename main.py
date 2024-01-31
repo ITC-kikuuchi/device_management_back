a1583a58e06348b96b92fb2a65b2bbe87b836883
kikuuchi <kikuuchi@itcowork.co.jp>
2024-01-31 20:04:12 +0900

    feat: fastApi app に サブルータの定義

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from routers import auth, pc, ios
+from routers import auth, pc, ios, android
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(pc.router)
 app.include_router(ios.router)
+app.include_router(android.router)
