8b8b63705adf34a676f957b50acdfca963cf7d39
kikuuchi <kikuuchi@itcowork.co.jp>
2024-02-01 20:03:44 +0900

    feat: Android 登録処理のリクエストスキーマの実装

--- schemas/t_android.py
+++ schemas/t_android.py
@@ -8,6 +8,9 @@ BaseModel は FastApi のスキーマモデルであることを示す。
 android クラスは BaseModel を継承しているクラス
 """
 
+# 日本時間のタイムゾーンの設定
+jst = timezone(timedelta(hours=9))
+
 class android(BaseModel):
     id: int
     label_name: Optional[str] = None
@@ -16,6 +19,25 @@ class android(BaseModel):
     type: Optional[str] = None
     os: Optional[str] = None
     delete_flag: bool
+
+    class Config:
+        orm_mode = True
+
+class createAndroid(BaseModel):
+    label_name: Optional[str] = None
+    android_name: Optional[str] = None
+    manufacturer: Optional[str] = None
+    type: Optional[str] = None
+    os: Optional[str] = None
+    carrier: Optional[str] = None
+    condition: Optional[str] = None
+    delivery_date: Optional[date] = None
+    disposal_date: Optional[date] = None
+    remarks: Optional[str] = None
+    location: Optional[str] = None
+    last_updated_flag: bool = Field(default=True)
+    created_at: Optional[datetime] = Field(default_factory=lambda: datetime.now(jst))
+    updated_at: Optional[datetime] = Field(default_factory=lambda: datetime.now(jst))
 
     class Config:
         orm_mode = True
