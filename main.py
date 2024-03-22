09e0674fdecd51d76e80b492798b2bae5a3b4698
kikuuchi <kikuuchi@itcowork.co.jp>
2024-03-22 10:49:11 +0900

    feat: ロギングの設定を行う処理の実装

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
 import logging
+
+# ロギングの設定
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
 from routers import auth, pc, ios, android, windows
 
 app = FastAPI()
