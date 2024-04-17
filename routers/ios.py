e3d90802b07a8aa37013f22be62045d1b4413ea7
kikuuchi <kikuuchi@itcowork.co.jp>
2024-04-17 09:49:09 +0900

    fix: コード整形

--- routers/ios.py
+++ routers/ios.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends,  HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from routers.auth import get_current_user
@@ -8,7 +8,8 @@ import cruds.t_ios as ios_crud
 import schemas.auth as auth_schema
 import cruds.auth as auth_crud
 
-router=APIRouter()
+router = APIRouter()
+
 
 # 最終更新フラグを false に変更
 def updateLastUpdateFlag(db: Session):
@@ -19,6 +20,7 @@ def updateLastUpdateFlag(db: Session):
         # 最終更新データの最終更新フラグの更新
         ios_crud.updateLastUpdateFlag(db, ios_data)
 
+
 # iOS一覧取得API
 @router.get("/ios", response_model=list[ios_schema.ios])
 def getIos(login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
@@ -27,9 +29,14 @@ def getIos(login_user: dict = Depends(get_current_user), db: Session = Depends(g
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 # iOS登録API
 @router.post("/ios")
-def createIos(ios: ios_schema.createIos, login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+def createIos(
+    ios: ios_schema.createIos,
+    login_user: dict = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
     try:
         # 最終更新フラグを false に変更
         updateLastUpdateFlag(db)
@@ -39,18 +46,29 @@ def createIos(ios: ios_schema.createIos, login_user: dict = Depends(get_current_
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 # iOS詳細取得API
 @router.get("/ios/{ios_id}", response_model=ios_schema.detailIos)
-def getIosDetail(ios_id: int, login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+def getIosDetail(
+    ios_id: int,
+    login_user: dict = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
     ios = ios_crud.getDetailIos(db, ios_id=ios_id)
     if not ios:
         # id に紐づくデータが存在しなかった場合
         raise HTTPException(status_code=404, detail=f"IOS_ID: {ios_id} not found")
     return ios
 
+
 # iOS更新API
 @router.put("/ios/{ios_id}")
-def updateIos(ios_id: int, ios: ios_schema.updateIos, login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+def updateIos(
+    ios_id: int,
+    ios: ios_schema.updateIos,
+    login_user: dict = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
     iosById = ios_crud.getDetailIos(db, ios_id=ios_id)
     if not iosById:
         # id に紐づくデータが存在しなかった場合
@@ -64,9 +82,14 @@ def updateIos(ios_id: int, ios: ios_schema.updateIos, login_user: dict = Depends
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 # iOS削除API
 @router.delete("/ios/{ios_id}")
-def deleteIos(ios_id: int, login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+def deleteIos(
+    ios_id: int,
+    login_user: dict = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
     iosById = ios_crud.getDetailIos(db, ios_id=ios_id)
     if not iosById:
         # id に紐づくデータが存在しなかった場合
@@ -76,10 +99,13 @@ def deleteIos(ios_id: int, login_user: dict = Depends(get_current_user), db: Ses
         return HTTPException(status_code=200)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
 # iOS最終更新者取得API
 @router.get("/ios_update_user", response_model=auth_schema.loginUser)
-def getUpdateUser(login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+def getUpdateUser(
+    login_user: dict = Depends(get_current_user), db: Session = Depends(get_db)
+):
     ios_data = ios_crud.getLastUpdatedData(db)
     if ios_data:
         return auth_crud.getUserById(db, ios_data.update_id)
