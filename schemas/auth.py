f0685c71dbeea8e73f10704b9c24b7d882120ff5
kikuuchi <kikuuchi@itcowork.co.jp>
2024-04-19 09:13:13 +0900

    fix: コード整形

--- schemas/auth.py
+++ schemas/auth.py
@@ -2,9 +2,11 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+
 class user(BaseModel):
     mail_address: Optional[str] = None
     password: Optional[str] = None
+
 
 class loginUser(BaseModel):
     id: int
